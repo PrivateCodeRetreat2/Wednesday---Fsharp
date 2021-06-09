b0a652313a4e3a043003b0a4c114b0a70a3af300
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-06-09 19:01:58 +0000

    end of day

--- GameOfLife/Tests.fs
+++ GameOfLife/Tests.fs
@@ -6,21 +6,17 @@ open VerifyTests
 open VerifyXunit
 open Newtonsoft.Json
 
+
 VerifierSettings.AddExtraSettings(fun settings ->
   settings.NullValueHandling <- NullValueHandling.Include)
 
-[<Fact>]
-let MyTest () =
-  async {
-    do! Verifier.Verify(15)
-          .ToTask() |> Async.AwaitTask
-  }
+let isAliveNextStep (alive:bool , numberOfNeighbors:int) = 
+  (alive && numberOfNeighbors = 2) || numberOfNeighbors = 3
 
 [<Fact>]
-let WithFluentSetting () =
+let RuleForCellAndNeighbors () =
   async {
-    do! Verifier.Verify(15)
-          .UseMethodName("customName")
-          .ToTask() |> Async.AwaitTask
+      let next = seq { for alive in [|false; true|] -> seq { for n in 1 .. 8 -> $"{alive},{n}  => {isAliveNextStep(alive , n)}"}}
+      do! Verifier.Verify(next)
+              .ToTask() |> Async.AwaitTask
   }
-do ()
